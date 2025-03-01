b17119b64d5f5ab12b71564d9eed4118f45eb15b
ArgenTemiraliev <temiraliev0304@gmail.com>
2025-03-02 00:58:00 +0600

    testemonials section

--- main.js
+++ main.js
@@ -5,4 +5,38 @@ menuOpenButton.addEventListener("click", () => {
 
 
 const menuClosenButton = document.querySelector("#menu-close-button") 
-menuClosenButton.addEventListener("click", () => menuOpenButton.click() )
+menuClosenButton.addEventListener("click", () => menuOpenButton.click())
+
+
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    gradCursor: true,
+    spaceBetween: 25,
+  
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+  
+  });
